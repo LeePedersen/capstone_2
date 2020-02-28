bd5b122240745d1b2373f3765ef209341ec9ad73
Lee Pedersen <leepedersenr@gmail.com>
2020-02-28 13:33:02 -0800

    add react components

---
+++ app/javascript/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home(props) {
+  return (
+    <div>
+      <h1>HOME</h1>
+      <p>It's working</p>
+    </div>
+  )
+}
+
+export default Home;
